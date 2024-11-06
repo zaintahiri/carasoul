9ea03a9a7381138af20ee4442d95fc60a172074b
zaintahiri <zain.tahiri940@gmail.com>
2024-11-06 09:39:39 +0500

    Carasoul has been made by zain ul abdin chandio

---
+++ src/Carasoul.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactDOM } from "react";
+import './CarasoulCss.css'
+function Carasoul(){
+    return(
+        <div className="container-img">
+
+        </div>
+    )
+}
+
+export default Carasoul;
+
